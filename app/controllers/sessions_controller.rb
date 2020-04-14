c76172de14368dbaa3944588c845de82cd4f1d02
Gregory280 <gregory280@hotmail.com>
2020-04-13 22:36:22 -0300

    fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
-  
+
   before_action :logged_in_redirect, only: [:new, :create]
-  
+
   def new
     
   end
@@ -25,13 +25,13 @@ class SessionsController < ApplicationController
   end
   
   private
-  
+
   def logged_in_redirect
     if logged_in?
       flash[:error] = 'You are already logged in.'
       redirect_to root_path
     end
   end
-  
+
 end
 
